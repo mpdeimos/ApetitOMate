54ad0f95c0fcc933796538f0f5167df29336ca78
Martin Pöhlmann <poehlmann@cqse.eu>
2019-02-11 08:56:27 +0100

    Azure function for todays table guests to slack

--- ApetitOMate.Function/DailyTableGuestsToSlack.cs
+++ ApetitOMate.Function/DailyTableGuestsToSlack.cs
@@ -1,5 +1,12 @@
 using System;
+using System.Threading.Tasks;
+using ApetitOMate.Core;
+using ApetitOMate.Core.Action;
+using ApetitOMate.Core.Api.Apetito;
+using ApetitOMate.Core.Api.Slack;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
 
@@ -8,9 +15,32 @@ namespace ApetitOMate.Function
     public static class DailyTableGuestsToSlack
     {
         [FunctionName(nameof(DailyTableGuestsToSlack))]
-        public static void Run([TimerTrigger("0 */5 * * * *")]TimerInfo myTimer, ILogger log)
+        public static async Task Run([TimerTrigger("0 0 11 * * *")]TimerInfo myTimer, ILogger log)
         {
-            log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+            log.LogInformation($"{nameof(DailyTableGuestsToSlack)} timer trigger function executed at: {DateTime.Now}");
+
+            try
+            {
+                Config config = Config.Instance;
+                await new TableGuestsToSlackAction(
+                    config,
+                    new ApetitoApiFactory(config.ApetitoConfig).Build(),
+                    new SlackApiFactory(config.SlackConfig).Build()
+                ).Run();
+            }
+            catch (Exception e)
+            {
+                log.LogError(e, "Could not perform perform task");
+                throw;
+            }
+        }
+
+        [FunctionName(nameof(DailyTableGuestsToSlack) + "_Http")]
+        public static async Task RunHttp(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = nameof(DailyTableGuestsToSlack))] HttpRequest req,
+            ILogger log)
+        {
+            await Run(null as TimerInfo, log);
         }
     }
 }
